8c2a7d3b2556d0fbb35eec3bd183cb09503b5586
Salubba <78054807+Salubba@users.noreply.github.com>
2021-02-25 13:00:49 +0100

    Mas codigo

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const URL_API = 'https://api.airtable.com/v0/appbkih2J6B816R5L/Productos?&view=Grid%20view';
 const URL_API_BORRAR = 'https://api.airtable.com/v0/appbkih2J6B816R5L/Productos?records[]=';
 const URL_API_ACTUALIZAR = 'https://api.airtable.com/v0/appbkih2J6B816R5L/Productos'
+const URL_API_CREAR = 'https://api.airtable.com/v0/appbkih2J6B816R5L/Productos'
 const AUTHORIZATION = 'Bearer keyYeYS1aqxUutFWK';
 
 
@@ -10,12 +11,37 @@ new Vue({
     data: {
         productos: [],
         //variable que guarda el texto de la nueva variable a actualizar
-        productoActualizar: ''
+        productoActualizar: '',
+        nuevoProducto: '',
+        productoEditar: false
+
     },
     mounted: function () {
         this.obtenerProductos();
     },
     methods: {
+        anyadirProducto: function () {
+            fetch(URL_API_CREAR, {
+                headers: {
+                    'Authorization': AUTHORIZATION,
+                    'Content-type': 'application/json'
+
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    "records": [
+                        {
+                            "fields": {
+                                "Nombre": this.nuevoProducto,
+                                "Adquirido": false
+                            }
+                        }
+                    ]
+                })
+            })
+                .then(() => this.nuevoProducto = '')
+                .then(() => this.obtenerProductos())
+        },
         obtenerProductos: function () {
             fetch(URL_API, {
                 headers: {
@@ -30,20 +56,39 @@ new Vue({
                     this.productos = json.records;
                 });
         },
-          borrarProducto: function (id) {
-              fetch(URL_API_BORRAR.concat(id), {
-                  headers: {
-                      'Authorization': AUTHORIZATION
-                  },
-                  method: 'DELETE'
-              });
-              //Borramos de local
-              this.productos = this.productos.filter(producto => {
-                  return producto.id !== id
-              });
+        borrarProducto: function (id) {
+            fetch(URL_API_BORRAR.concat(id), {
+                headers: {
+                    'Authorization': AUTHORIZATION
+                },
+                method: 'DELETE'
+            });
+            //Borramos de local
+            this.productos = this.productos.filter(producto => {
+                return producto.id !== id
+            });
 
-          },
-          actualizarAdquiridoEnApi: function (id, checked) {
+        },
+        actualizarProductoEnAPI: function(id, nuevoTexto) {
+            fetch(URL_API_UPDATE, {
+                headers: {
+                    'Content-type': 'application/json',
+                    'Authorization': AUTHORIZATION
+                },
+                method: 'PATCH',
+                body: JSON.stringify({
+                    "records": [
+                        {
+                            "id": id,
+                            "fields": {
+                                "Nombre": nuevoTexto,
+                            }
+                        }
+                    ]
+                })
+            })
+        },
+        actualizarAdquiridoEnApi: function (id, checked) {
             fetch(URL_API_ACTUALIZAR, {
                 headers: {
                     'Content-type': 'application/json',
@@ -61,19 +106,23 @@ new Vue({
                     ]
                 })
             })
-              //Actualizamos en local
-              this.productos = this.productos.map((producto) => {
-                  if (producto.id === id) {
-                      let miProducto = producto;
-                      producto.fields.Adquirido = checked;
-                      return miProducto;
-                  } else {
-                      return producto;
-                  }
+            //Actualizamos en local
+            this.productos = this.productos.map((producto) => {
+                if (producto.id === id) {
+                    let miProducto = producto;
+                    producto.fields.Adquirido = checked;
+                    return miProducto;
+                } else {
+                    return producto;
+                }
 
-              });
+            });
 
+        },
+        abrirEditar: function (id, nombre) {
+            this.productoEditar = id;
+            this.productoActualizar = nombre;
         }
-
     }
+
 })
